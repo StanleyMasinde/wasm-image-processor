9676e7c800dfe6411d3bdf428c49f2009139cfe3
Stanley Masinde <stanmasinde@gmail.com>
2025-08-23 02:03:52 +0300

    update the ppackage definition

--- demo/wasm_image_processor.js
+++ demo/wasm_image_processor.js
@@ -136,7 +136,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('pwa_image_generator_bg.wasm', import.meta.url);
+        module_or_path = new URL('wasm_image_processor_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
