b295132031b55655587151af88ca877ee69f9a8b
Stanley Masinde <stanmasinde@gmail.com>
2025-08-23 03:48:35 +0300

    update the docs

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,13 @@ extern "C" {
     pub fn log(m: &str);
 }
 
+///
+/// Resize an image by the given dimension.
+/// The first parameter is an array of bytes.
+/// The second parameter is the size of the sides.
+/// This function returns square images. It does not respect the aspect
+/// Ratio of the photo.
+/// It is is ideal for icon resizing.
 #[wasm_bindgen]
 pub fn resize_image(image_data: Vec<u8>, side: u32) -> Vec<u8> {
     let image = read_image(image_data).unwrap();
@@ -25,6 +32,13 @@ pub fn resize_image(image_data: Vec<u8>, side: u32) -> Vec<u8> {
     buf
 }
 
+///
+/// Read image file from a stream of bytes.
+/// This function returns `DynamicImage` so that it can be processed.
+///
+/// The reason we are passing a Vec<u8> is because wasm_bindgen can pass it to JS.
+/// It cannot directly pass DynamicImage to JS.
+///
 pub fn read_image(image_data: Vec<u8>) -> Result<DynamicImage, Box<dyn Error>> {
     let img = ImageReader::new(Cursor::new(image_data))
         .with_guessed_format()?
